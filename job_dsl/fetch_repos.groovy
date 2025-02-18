1f70028032d01bc902d272e75aa8c1eb392e24f0
syuhas <syuhas22@gmail.com>
2025-02-18 12:06:52 -0500

    trying now git urls

--- job_dsl/fetch_repos.groovy
+++ job_dsl/fetch_repos.groovy
@@ -1,53 +1,18 @@
 job('test_fetch_repos') {
-    description('Job to fetch a list of repositories and branches from a GitHub account.')
-    
+    description('Fetches repositories from GitHub and lists branches')
+
     parameters {
         stringParam('GITHUB_USER', 'syuhas', 'GitHub organization or user from which to fetch repositories')
     }
 
-    properties {
-        githubProjectUrl('https://github.com/syuhas/repo-select/')
+    environmentVariables {
+        env('TOKEN', credentials('GITHUB_TOKEN'))
     }
 
-    scm {
-        git {
-            remote {
-                url('https://github.com/syuhas/repo-select.git')
-                credentials('GITHUB_TOKEN') // Use the stored credential ID
-            }
-            branch('*/main')
-        }
-    }
-
-    wrappers {
-        credentialsBinding {
-            string('TOKEN', 'GITHUB_TOKEN') // Injects the GitHub Token into the environment
-        }
-    }
-
-    definition {
-        cps {
-            script("""
-            pipeline {
-                agent any
-
-                environment {
-                    GITHUB_USER = "\${params.GITHUB_USER}"
-                    TOKEN = credentials('GITHUB_TOKEN') // Correctly reference Jenkins stored credential
-                }
-
-                stages {
-                    stage('Fetch Repositories') {
-                        steps {
-                            script {
-                                sh 'chmod +x deploy/repos.sh'
-                                sh './deploy/repos.sh'
-                            }
-                        }
-                    }
-                }
-            }
-            """)
-        }
+    steps {
+        shell('''
+            chmod +x deploy/repos.sh
+            ./deploy/repos.sh
+        ''')
     }
 }
